42a8950a1501757a6d18ab965845e0b791f0694c
Cobub <cobub.open@gmail.com>
2012-10-18 22:34:24 +0800

    0.2

--- web/assets/js/jquery.select.js
+++ web/assets/js/jquery.select.js
@@ -8,11 +8,26 @@ function initTimeSelect()
     $(document).click(function(e){
     	e.stopPropagation();
     	 if($(e.target).closest('#ui-datepicker-div').length>0|$(e.target).closest('.compared_date_select').length>0||$(e.target).closest('.datainp').length>0||$(e.target).closest('#calroot').length>0){//calroot
-    		 	return false;
+    		 {
+    			 return false;
+    		 }	
+    		 	
     	 }
+    	 
+    	if($(e.target).closest('.ui-datepicker-prev').length >0)
+    	{
+    		 return false;
+    	}
+    	
+    	if($(e.target).closest('.ui-datepicker-next').length >0)
+    	{
+    		 return false;
+    	}
+    	
     	$('.select_list_body').slideUp(200);
     });
-    $('body').delegate('.select_list_body ul li.date_picker','click',function(e){
+    
+    $('body').delegate('.select_list_body ul li.date_picker  ','click',function(e){
     	e.stopPropagation();
     	$(this).next('li').slideToggle(200);
     });	
