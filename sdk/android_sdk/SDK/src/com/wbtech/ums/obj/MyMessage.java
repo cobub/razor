b08201b31eaa92e9098659bdd875c55017a6e0e2
Cobub <cobub.open@gmail.com>
2012-11-10 20:45:36 +0800

    missing files in android sdk

---
+++ sdk/android_sdk/SDK/src/com/wbtech/ums/obj/MyMessage.java
@@ -0,0 +1,32 @@
+/**
+ * Cobub Razor
+ *
+ * An open source analytics android sdk for mobile applications
+ *
+ * @package		Cobub Razor
+ * @author		WBTECH Dev Team
+ * @copyright	Copyright (c) 2011 - 2012, NanJing Western Bridge Co.,Ltd.
+ * @license		http://www.cobub.com/products/cobub-razor/license
+ * @link		http://www.cobub.com/products/cobub-razor/
+ * @since		Version 0.1
+ * @filesource
+ */
+package com.wbtech.ums.obj;
+
+public class MyMessage {
+	public  boolean flag;
+	public  String msg;
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
