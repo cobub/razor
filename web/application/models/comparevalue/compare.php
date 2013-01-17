0f337a382865e681d63970f0be762d06a29e6ed8
Cobub <cobub.open@gmail.com>
2013-01-17 10:31:51 +0800

    Fix alert error

--- web/application/models/comparevalue/compare.php
+++ web/application/models/comparevalue/compare.php
@@ -20,18 +20,15 @@ class Compare extends CI_Model {
 	function getAllAlertlab(){
 		$userId = $this->common->getUserId();
 		$product = $this->common->getCurrentProduct();
-// 		$sql = "select label,`condition`  from ".$this->db->dbprefix('alert')." where userid='".$userId."' and productid=".$product->id."";
-		$sql = "select *  from razor_alert where userid='15' and productid=1";
-// 		echo $sql;
+	    $sql = "select * from ".$this->db->dbprefix('alert')."";
 		$result = $this->db->query($sql);
-// 		print_r($result->result_array());
-		  return $result->result_array();  
+		return $result->result_array();  
 	}
 	function addAlertEmail($alertlabel,$factdata,$forecastdata,$time,$states){
 		
 		$states=1;
 		$sql="INSERT INTO ".$this->db->dbprefix('alertdetail')." (  `states` ,  `time` ,  `forecastdata` ,  `factdata` ,  `alertlabel` ) 
-VALUES ( $states,  '$time', $forecastdata, $factdata,  '$alertlabel' );";
+        VALUES ( $states,  '$time', $forecastdata, $factdata,  '$alertlabel' );";
 		$this->db->query($sql);
 	}
 	
