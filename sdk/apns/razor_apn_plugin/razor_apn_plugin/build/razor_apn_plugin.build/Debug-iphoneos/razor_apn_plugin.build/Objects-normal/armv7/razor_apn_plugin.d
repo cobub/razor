be2da7e5f3ac32a1281dc90813c2b7b3f9cc5309
guowseu <guowseu@gmail.com>
2013-09-24 15:27:36 +0800

    Add apns ios push sdk

---
+++ sdk/apns/razor_apn_plugin/razor_apn_plugin/build/razor_apn_plugin.build/Debug-iphoneos/razor_apn_plugin.build/Objects-normal/armv7/razor_apn_plugin.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/wb/Projects/razor/sdk/apns/razor_apn_plugin/razor_apn_plugin/razor_apn_plugin/razor_apn_plugin.m \
+  /Users/wb/Projects/razor/sdk/apns/razor_apn_plugin/razor_apn_plugin/razor_apn_plugin/razor_apn_plugin.h \
+  /Users/wb/Projects/razor/sdk/apns/razor_apn_plugin/razor_apn_plugin/razor_apn_plugin/razor_apn_network.h
