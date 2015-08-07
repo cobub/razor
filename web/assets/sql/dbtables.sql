b146c140a6c2e33fe61cd3b07ceeba2b1ed7dfac
brucenan <brucenan@gmail.com>
2015-08-07 15:00:16 +0800

    update ci_sessions ip_address width according to CI upgrade manual

--- web/assets/sql/dbtables.sql
+++ web/assets/sql/dbtables.sql
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `umsinstall_channel_product` (
 
 CREATE TABLE IF NOT EXISTS `umsinstall_ci_sessions` (
   `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) COLLATE utf8_bin NOT NULL DEFAULT '0',
   `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
   `user_data` text COLLATE utf8_bin NOT NULL,
