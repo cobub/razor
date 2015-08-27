f22de153ee29fc6c08cd7714bb400a0f2b3fa8b0
zhang <dd_zhangdan@163.com>
2015-08-27 14:36:34 +0800

    modify the format
    Reviewers: nan.li
    
    Reviewed By: nan.li
    
    Differential Revision: http://pha.cobub.cn/D114

--- web/application/models/pluginm.php
+++ web/application/models/pluginm.php
@@ -1,115 +1,158 @@
 <?php
+/**
+ * Cobub Razor
+ *
+ * An open source mobile analytics system
+ *
+ * PHP versions 5
+ *
+ * @category  MobileAnalytics
+ * @package   CobubRazor
+ * @author    Cobub Team <open.cobub@gmail.com>
+ * @copyright 2011-2016 NanJing Western Bridge Co.,Ltd.
+ * @license   http://www.cobub.com/docs/en:razor:license GPL Version 3
+ * @link      http://www.cobub.com
+ * @since     Version 0.1
+ */
 
-class pluginM extends CI_Model {
-	function __construct()
-	{
-		
-		parent::__construct();
-		$this->load->helper('file');
-		$this->load->helper('path');
-	}
+/**
+ * Plugin Model
+ *
+ * @category PHP
+ * @package  Model
+ * @author   Cobub Team <open.cobub@gmail.com>
+ * @license  http://www.cobub.com/docs/en:razor:license GPL Version 3
+ * @link     http://www.cobub.com
+ */
+class PluginM extends CI_Model
+{
+    /**
+     * Construct funciton, to pre-load database configuration
+     *
+     * @return void
+     */
+    function __construct ()
+    {
+        parent::__construct();
+        $this->load->helper('file');
+        $this->load->helper('path');
+    }
 
-	 function traverse($path ,$returnarr) {
-	 	
-	 		$entity = array();
-			$classname=array();
+    /**
+     * traverse function
+     * traverse
+     *
+     * @param string $path      path
+     * @param string $returnarr return arr
+     *
+     * @return query returnarr
+     */
+    function traverse ($path, $returnarr)
+    {
+        $entity = array();
+        $classname = array();
+        
+        $current_dir = opendir($path); // opendir()返回一个目录句柄,失败返回false
+        while (($file = readdir($current_dir)) !== false) { // readdir()返回打开目录句柄中的一个条目
+            
+            $sub_dir = $path . $file; // 构建子目录路径
+            
+            if ($file == '.' || $file == '..' || $file == '.svn') {
+                continue;
+            } else {
+                if (is_dir($sub_dir)) {
+                    $returnarr = $this->traverse($sub_dir . "/", $returnarr);
+                } else {
+                    $index = strrpos($file, ".");
+                    $filename = substr($file, 0, $index);
+                    
+                    $entity = array(
+                            'classpath' => $path . $file,
+                            'classname' => $filename
+                    );
+                    array_push($returnarr, $entity);
+                }
+            }
+        }
+        return $returnarr;
+    }
 
-
-                $current_dir = opendir($path);    //opendir()返回一个目录句柄,失败返回false
-                while(($file = readdir($current_dir)) !== false) {    //readdir()返回打开目录句柄中的一个条目
-                	
-                   $sub_dir = $path.  $file;    //构建子目录路径
-
-                    if($file == '.' || $file == '..'||$file=='.svn') {
-                        continue;
-                    } else {if(is_dir($sub_dir)) {   
-                     $returnarr=$this->traverse($sub_dir."/",$returnarr);
-                   } else {   
-                       	$index=strrpos ($file,".");
-						$filename=substr($file,0,$index);
-                      
-                       	$entity=array(
-                       		'classpath'=>$path  . $file,
-							'classname'=>$filename);
-                       	array_push($returnarr, $entity);
-                   }}
-               }
-               return $returnarr;
-           }
-  
-	
-	function run($functionname,$par){
-		$dir=dirname(dirname(__FILE__))."/plugin/";
-		$returnarr=array();
-		$arr=$this->traverse($dir,$returnarr);
-		$pluginsArray = array();
-		for($i=0;$i<count($arr);$i++){
-			$classpat =  $arr[$i]['classpath'];
-			require_once ($classpat);
-			$classname=$arr[$i]['classname'];
-			if($classname=="pluginInterface"){
-				continue;
-			}
-			$reflectionClass = new ReflectionClass($classname);
-			if ($reflectionClass->implementsInterface('pluginInterface')) {
-				try {
-					$cla= $reflectionClass->newInstance();
-				} catch (Exception $e) {
-					continue;
-				}
-				
-				if(method_exists($cla, $functionname)){
-					$function=	$reflectionClass->getmethod($functionname);
-// 					$this->runfunction($classname,$classpat, "getName", $par);
-					$str=$function->invoke($cla,$par);
-					array_push($pluginsArray,$str);
-				}
-			}
-		}
-		return $pluginsArray;
-	}
-	
-	
-	
-	
-	// function runfunction($classname,$classpat,$functionname,$par){
-		
-		
-		
-	// 		require_once ($classpat);
-	// 		$reflectionClass = new ReflectionClass($classname);
-	// 			try {
-	// 				$cla= $reflectionClass->newInstance();
-	// 			} catch (Exception $e) {
-	// 				continue;
-	// 			}
-		
-	// 			if(method_exists($cla, $functionname)){
-	// 				$function=	$reflectionClass->getmethod($functionname);
-	// 				$str=$function->invoke($cla,$par);
-	// 				echo $str."<br>";
-	// 			}
-		
-	// }
-	
-	// function get_ini_file($file_name = "demo.ini"){
-	// 	$str=file_get_contents($file_name);
-	// 	$ini_list = explode("\r\n",$str);
-	// 	$ini_items = array();
-	// 	foreach($ini_list as $item){
-	// 		$one_item = explode("=",$item);
-	// 		if(isset($one_item[0])&&isset($one_item[1])) $ini_items[trim($one_item[0])] = trim($one_item[1]); //���key=>value����ʽ.
-	// 	}
-	// 	return $ini_items;
-	// }
-	
-	// function get_ini_item($ini_items = null,$item_name = ''){
-	// 	if(empty($ini_items)) return "";
-	// 	else return $ini_items[$item_name];
-	// }
-	
-	
-	
+    /**
+     * run function
+     * run
+     *
+     * @param string $functionname function name
+     * @param string $par          par
+     *
+     * @return query pluginsArray
+     */
+    function run ($functionname, $par)
+    {
+        $dir = dirname(dirname(__FILE__)) . "/plugin/";
+        $returnarr = array();
+        $arr = $this->traverse($dir, $returnarr);
+        $pluginsArray = array();
+        for ($i = 0; $i < count($arr); $i ++) {
+            $classpat = $arr[$i]['classpath'];
+            require_once ($classpat);
+            $classname = $arr[$i]['classname'];
+            if ($classname == "pluginInterface") {
+                continue;
+            }
+            $reflectionClass = new ReflectionClass($classname);
+            if ($reflectionClass->implementsInterface('pluginInterface')) {
+                try {
+                    $cla = $reflectionClass->newInstance();
+                } catch (Exception $e) {
+                    continue;
+                }
+                
+                if (method_exists($cla, $functionname)) {
+                    $function = $reflectionClass->getmethod($functionname);
+                    // $this->runfunction($classname,$classpat, "getName",
+                    // $par);
+                    $str = $function->invoke($cla, $par);
+                    array_push($pluginsArray, $str);
+                }
+            }
+        }
+        return $pluginsArray;
+    }
+    
+    // function runfunction($classname,$classpat,$functionname,$par){
+    
+    // require_once ($classpat);
+    // $reflectionClass = new ReflectionClass($classname);
+    // try {
+    // $cla= $reflectionClass->newInstance();
+    // } catch (Exception $e) {
+    // continue;
+    // }
+    
+    // if(method_exists($cla, $functionname)){
+    // $function= $reflectionClass->getmethod($functionname);
+    // $str=$function->invoke($cla,$par);
+    // echo $str."<br>";
+    // }
+    
+    // }
+    
+    // function get_ini_file($file_name = "demo.ini"){
+    // $str=file_get_contents($file_name);
+    // $ini_list = explode("\r\n",$str);
+    // $ini_items = array();
+    // foreach($ini_list as $item){
+    // $one_item = explode("=",$item);
+    // if(isset($one_item[0])&&isset($one_item[1]))
+    // $ini_items[trim($one_item[0])] = trim($one_item[1]); //���key=>value����ʽ.
+    // }
+    // return $ini_items;
+    // }
+    
+    // function get_ini_item($ini_items = null,$item_name = ''){
+    // if(empty($ini_items)) return "";
+    // else return $ini_items[$item_name];
+    // }
 }
 
 ?>
--- web/application/models/point_mark.php
+++ web/application/models/point_mark.php
@@ -1,95 +1,220 @@
-<?php 
+<?php
 /**
  * Cobub Razor
  *
- * An open source analytics for mobile applications
+ * An open source mobile analytics system
  *
- * @package		Cobub Razor
- * @author		WBTECH Dev Team
- * @copyright	Copyright (c) 2011 - 2012, NanJing Western Bridge Co.,Ltd.
- * @license		http://www.cobub.com/products/cobub-razor/license
- * @link		http://www.cobub.com/products/cobub-razor/
- * @since		Version 3.0
- * @filesource
+ * PHP versions 5
+ *
+ * @category  MobileAnalytics
+ * @package   CobubRazor
+ * @author    Cobub Team <open.cobub@gmail.com>
+ * @copyright 2011-2016 NanJing Western Bridge Co.,Ltd.
+ * @license   http://www.cobub.com/docs/en:razor:license GPL Version 3
+ * @link      http://www.cobub.com
+ * @since     Version 0.1
  */
- class Point_mark extends CI_Model{
- 
-	function __construct(){
-		parent::__construct();
-		$this->load->database();
-	}
-	
-	//add a point mark
-	public function addPointmark($data=array()){
-		$this->db->insert($this->db->dbprefix('markevent'),$data);
-		return $this->db->affected_rows();
-	}
-	//remove a point mark
-	public function removePointmark($userid,$productid,$date){
-		$sql='delete from razor_markevent where userid='.$userid.' and productid='.$productid.' and marktime='.'"'.$date.'"';
-		return $this->db->query($sql);
-	}
-	//return a point list and content to charts
-	public function listPointviewtochart($userid,$productid,$fromdate,$enddate,$type=''){
-		$sql='select u.username,m.userid,m.title,m.description,m.marktime,m.private from '.$this->db->dbprefix('markevent').' m
-left join '.$this->db->dbprefix('users').' u on m.userid=u.id
-LEFT JOIN '.$this->db->dbprefix('product').' p on p.id=m.productid
-where m.marktime BETWEEN "'.$fromdate.'" and "'.$enddate.'" and p.id='.$productid.' and m.userid='.$userid.' or(m.userid!='.$userid.' and private=1 and p.id='.$productid.') GROUP BY u.username,m.userid,m.title,m.description,m.marktime,m.private ORDER BY m.marktime asc';
-		if('listcount'==$type){
-		$sql='select count(1) c,m.userid from '.$this->db->dbprefix('markevent').' m
-		left join '.$this->db->dbprefix('users').' u on m.userid=u.id
-		LEFT JOIN '.$this->db->dbprefix('product').' p on p.id=m.productid
-		where m.marktime BETWEEN "'.$fromdate.'" and "'.$enddate.'" and p.id='.$productid.' and m.userid='.$userid.' or(m.userid!='.$userid.' and private=1) GROUP BY m.userid';
-		return $this->db->query($sql)->result_array();
-		}
-		return $this->db->query($sql);
-	}
-	
-	//modify a point mark
-	public function modifyPointmark($data=array(),$userid,$productid,$markdate){
-		$where = 'userid = '.$userid.' AND productid = '.$productid.' and marktime="'.$markdate.'"';
-		$sql = $this->db->update_string($this->db->dbprefix('markevent'), $data, $where);
-		$this->db->query($sql);
-		return $this->db->affected_rows();
-	}
-	//manage all point marks
-	public function managePointmarkpagelist($userid,$productid,$fromTime,$toreTime){
-		$sql='select m.id,u.username,m.userid,m.title,m.description,m.marktime,m.private from '.$this->db->dbprefix('markevent').' m
-left join '.$this->db->dbprefix('users').' u on m.userid=u.id
-LEFT JOIN '.$this->db->dbprefix('product').' p on p.id=m.productid
-where p.id='.$productid.' and m.userid='.$userid.' and m.marktime BETWEEN \''.$fromTime.'\' and \''.$toreTime.'\'';
-		return $this->db->query($sql);
-	}
- 
-	// check the same date for user whether if insert
-	public function ifcaninsert($userid, $productid, $date) {
-		$query = $this->db->query ( 'SELECT * FROM '.$this->db->dbprefix('markevent').' WHERE userid=' . $userid . ' AND productid=' . $productid . ' AND marktime="'.$date.'"' );
-		$count= $query->num_rows();
-		if($count>=1){
-			return false;
-		}
-		return true;
-	}
-	
-	function timediff( $begin_time, $end_time )
-	{
-		if ( $begin_time < $end_time ) {
-			$starttime = $begin_time;
-			$endtime = $end_time;
-		} else {
-			$starttime = $end_time;
-			$endtime = $begin_time;
-		}
-		$timediff = $endtime - $starttime;
-		$days = intval( $timediff / 86400 );
-		$remain = $timediff % 86400;
-		$hours = intval( $remain / 3600 );
-		$remain = $remain % 3600;
-		$mins = intval( $remain / 60 );
-		$secs = $remain % 60;
-		$res = array( "day" => $days, "hour" => $hours, "min" => $mins, "sec" => $secs );
-		return $res;
-	}
-	
- }
+
+/**
+ * Point_Mark Model
+ *
+ * @category PHP
+ * @package  Model
+ * @author   Cobub Team <open.cobub@gmail.com>
+ * @license  http://www.cobub.com/docs/en:razor:license GPL Version 3
+ * @link     http://www.cobub.com
+ */
+class Point_Mark extends CI_Model
+{
+    /**
+     * Construct funciton, to pre-load database configuration
+     *
+     * @return void
+     */
+    function __construct ()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    /**
+     * addPointmark function
+     * add a point mark
+     *
+     * @param array $data data
+     *
+     * @return query affected_rows
+     */
+    public function addPointmark ($data = array())
+    {
+        $this->db->insert($this->db->dbprefix('markevent'), $data);
+        return $this->db->affected_rows();
+    }
+    
+    /**
+     * removePointmark function
+     * remove a point mark
+     *
+     * @param int   $userid    user id
+     * @param int   $productid product id
+     * @param array $date      date
+     *
+     * @return query sql
+     */
+    public function removePointmark ($userid, $productid, $date)
+    {
+        $sql = 'delete from razor_markevent where userid=' . $userid .
+                 ' and productid=' . $productid . ' and marktime=' . '"' . $date .
+                 '"';
+        return $this->db->query($sql);
+    }
+    
+    /**
+     * listPointviewtochart function
+     * return a point list and content to charts
+     *
+     * @param int    $userid    user id
+     * @param int    $productid product id
+     * @param string $fromdate  from date
+     * @param string $enddate   end date
+     * @param int    $type      type
+     *
+     * @return query result_array
+     */
+    public function listPointviewtochart ($userid, $productid, $fromdate, $enddate, $type = '')
+    {
+        $sql = 'select u.username,m.userid,m.title,m.description,m.marktime,m.private from ' .
+                 $this->db->dbprefix('markevent') . ' m
+                 left join ' .
+                 $this->db->dbprefix('users') . ' u on m.userid=u.id
+                 LEFT JOIN ' .
+                 $this->db->dbprefix('product') .
+                 ' p on p.id=m.productid
+                 where m.marktime BETWEEN "' .
+                 $fromdate . '" and "' . $enddate . '" and p.id=' . $productid .
+                 ' and m.userid=' . $userid . ' or(m.userid!=' . $userid .
+                 ' and private=1 and p.id=' . $productid .
+                 ') GROUP BY u.username,m.userid,m.title,m.description,m.marktime,m.private ORDER BY m.marktime asc';
+        if ('listcount' == $type) {
+            $sql = 'select count(1) c,m.userid from ' .
+                     $this->db->dbprefix('markevent') . ' m
+		left join ' .
+                     $this->db->dbprefix('users') . ' u on m.userid=u.id
+		LEFT JOIN ' .
+                     $this->db->dbprefix('product') .
+                     ' p on p.id=m.productid
+		where m.marktime BETWEEN "' .
+                     $fromdate . '" and "' . $enddate . '" and p.id=' .
+                     $productid . ' and m.userid=' . $userid . ' or(m.userid!=' .
+                     $userid . ' and private=1) GROUP BY m.userid';
+            return $this->db->query($sql)->result_array();
+        }
+        return $this->db->query($sql);
+    }
+    
+    /**
+     * modifyPointmark function
+     * modify a point mark
+     *
+     * @param int    $userid    user id
+     * @param int    $productid product id
+     * @param string $markdate  markdate
+     * @param array  $data      data
+     *
+     * @return query affected_rows
+     */
+    public function modifyPointmark ($userid, $productid, $markdate, $data = array())
+    {
+        $where = 'userid = ' . $userid . ' AND productid = ' . $productid .
+                 ' and marktime="' . $markdate . '"';
+        $sql = $this->db->update_string($this->db->dbprefix('markevent'), $data, $where);
+        $this->db->query($sql);
+        return $this->db->affected_rows();
+    }
+    
+    /**
+     * managePointmarkpagelist function
+     * manage all point marks
+     *
+     * @param int    $userid    user id
+     * @param int    $productid product id
+     * @param string $fromTime  from time
+     * @param string $toreTime  tore time
+     *
+     * @return query sql
+     */
+    public function managePointmarkpagelist ($userid, $productid, $fromTime, $toreTime)
+    {
+        $sql = 'select m.id,u.username,m.userid,m.title,m.description,m.marktime,m.private from ' .
+                 $this->db->dbprefix('markevent') . ' m
+                left join ' .
+                 $this->db->dbprefix('users') . ' u on m.userid=u.id
+                 LEFT JOIN ' .
+                 $this->db->dbprefix('product') .
+                 ' p on p.id=m.productid
+                 where p.id=' .
+                 $productid . ' and m.userid=' . $userid .
+                 ' and m.marktime BETWEEN \'' . $fromTime . '\' and \'' .
+                 $toreTime . '\'';
+        return $this->db->query($sql);
+    }
+    
+    /**
+     * ifcaninsert function
+     * check the same date for user whether if insert
+     *
+     * @param int    $userid    user id
+     * @param int    $productid product id
+     * @param string $date      date
+     *
+     * @return bool
+     */
+    public function ifcaninsert ($userid, $productid, $date)
+    {
+        $query = $this->db->query(
+            'SELECT * FROM ' . $this->db->dbprefix('markevent') .
+            ' WHERE userid=' . $userid . ' AND productid=' .
+            $productid . ' AND marktime="' . $date . '"'
+        );
+        $count = $query->num_rows();
+        if ($count >= 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * timediff function
+     * time diff
+     *
+     * @param string $begin_time begin time
+     * @param string $end_time   end time
+     *
+     * @return query res
+     */
+    function timediff ($begin_time, $end_time)
+    {
+        if ($begin_time < $end_time) {
+            $starttime = $begin_time;
+            $endtime = $end_time;
+        } else {
+            $starttime = $end_time;
+            $endtime = $begin_time;
+        }
+        $timediff = $endtime - $starttime;
+        $days = intval($timediff / 86400);
+        $remain = $timediff % 86400;
+        $hours = intval($remain / 3600);
+        $remain = $remain % 3600;
+        $mins = intval($remain / 60);
+        $secs = $remain % 60;
+        $res = array(
+                "day" => $days,
+                "hour" => $hours,
+                "min" => $mins,
+                "sec" => $secs
+        );
+        return $res;
+    }
+}
 ?>
